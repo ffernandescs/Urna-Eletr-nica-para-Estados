a0cf634a4ab19f14aeb4131495752bc61ace8ffc
Felipe Fernandes <f.fernandescs@gmail.com>
2022-03-22 13:34:25 -0300

    Commit de Projeto finalizdo em JS

---
+++ etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                name: 'Felipe Fernandes',
+                partido: 'Bolsonarista',
+                fotos:[
+                    {url:'logo.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '38112',
+                name: 'Programador Dev',
+                partido: 'Capitalista',
+                fotos:[
+                    {url:'logo.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                name: 'Luladrão',
+                partido: 'Pão com Mortadela',
+                fotos:[
+                    {url:'logo.jpg', legenda: 'Prefeito'},
+                    {url:'logo.jpg', legenda: 'Vice-Prefeito'}
+                ]
+            },
+            {
+                numero: '17',
+                name: 'Bolsonaro',
+                partido: 'Bolsonarista',
+                fotos:[
+                    {url:'logo.jpg', legenda: 'Prefeito'},
+                    {url:'logo.jpg', legenda: 'Vice-Prefeito'}
+                ]
+            },
+        ]
+    },
+]
